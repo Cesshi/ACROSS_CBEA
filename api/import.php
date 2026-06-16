d7a975d0cd03c4d4a50f06deaaf3c12452b64089
Cesshi <invencionceciliamarie@gmail.com>
2026-06-16 11:04:03 +0800

    fixed errors in import

--- api/import.php
+++ api/import.php
@@ -1,5 +1,5 @@
 <?php
-ini_set('display_errors', 1); error_reporting(E_ALL);
+ini_set('display_errors', 0); error_reporting(0);
 session_start();
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
